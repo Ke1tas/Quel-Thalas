8fe1d9fc1fdeedf65e3fb491a0cbbd0690434265
Keltas <inozemtseeev@gmail.com>
2024-10-19 13:30:09 +0400

    add exeption to file_reader

--- lab/lab2.py
+++ lab/lab2.py
@@ -45,6 +45,8 @@ def file_reader(annotation_file: str) -> list:
     :param annotation_file: путь к файлу
     :return: массив строк из файла
     """
+    if not os.path.exists(annotation_file):
+        raise FileNotFoundError(f"Файл аннотации не найден: {annotation_file}")
     annotations = []
     with open(annotation_file, mode='r', newline='', encoding='utf-8') as file:
         reader = csv.reader(file)
