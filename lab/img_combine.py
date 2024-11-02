c2243b4e72badf06f3a63ebf692c51ee94e4b445
Keltas <inozemtseeev@gmail.com>
2024-11-02 15:05:41 +0400

    removed parentheses from if condition

--- lab/img_combine.py
+++ lab/img_combine.py
@@ -8,7 +8,7 @@ def combine_images(img1:np.ndarray,img2:np.ndarray)->np.ndarray:
     :param img2: Правое изображение
     :return: Соединенные 2 изображения
     """
-    if(img1.shape[1] != img2.shape[1]):
+    if img1.shape[1] != img2.shape[1]:
         raise ValueError(f"Изображения имеют разную высоту: {img1} , {img2}")
     combined_img = np.hstack((img1, img2))
     return combined_img
