670532076cd5fda0221bccab762d045ed839a3ae
Keltas <inozemtseeev@gmail.com>
2024-11-15 20:55:05 +0400

    разделил код на функции

--- lab/lab4.py
+++ lab/lab4.py
@@ -1,39 +1,52 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import csv
 import cv2
-
-df = pd.read_csv('annot')
-df.columns = ['absolute_path','relative_path']
+import os
 
 
-h = []
-w = []
-d = []
-for i in df["absolute_path"]:
-    shape = cv2.imread(i).shape
-    h.append(shape[0])
-    w.append(shape[1])
-    d.append(shape[2])
-df.insert(2, "height", pd.Series(h), True)
-df.insert(3, "width", pd.Series(w), True)
-df.insert(4, "depth", pd.Series(d), True)
+def init_data_frame(annotation_path:str)-> pd.DataFrame:
+    if not os.path.exists(annotation_path):
+        raise FileNotFoundError(f"Файл аннотации не найден: {annotation_path}")
+    df = pd.read_csv(annotation_path)
+    df.columns = ['absolute_path', 'relative_path']
+    return df
 
+def add_h_w_d_cols(df:pd.DataFrame)->None:
+    h = []
+    w = []
+    d = []
+    for i in df["absolute_path"]:
+        shape = cv2.imread(i).shape
+        h.append(shape[0])
+        w.append(shape[1])
+        d.append(shape[2])
+    df.insert(2, "height", pd.Series(h), True)
+    df.insert(3, "width", pd.Series(w), True)
+    df.insert(4, "depth", pd.Series(d), True)
 
-stats = df[['height', 'width', 'depth']].describe()
-print(stats)
-def filter_images(dataframe, max_width, max_height):
-    return dataframe[(dataframe['width'] <= max_width) & (dataframe['height'] <= max_height)]
-df["area"] = df["height"]*df["width"]
-df_filtered = filter_images(df,500,500)
-print(df_filtered)
-df_sorted = df.sort_values(by='area')
-print(df_sorted)
+def h_w_d_statistical_info(df:pd.DataFrame)->None:
+    stats = df[['height', 'width', 'depth']].describe()
+    print(stats)
 
-plt.figure(figsize=(10, 5))
-plt.hist(df['area'], color='gray')
-plt.title('Распределение площадей изображений')
-plt.xlabel('Площадь (пиксели)')
-plt.ylabel('Частота')
-plt.grid(axis='y', alpha=0.75)
-plt.show()
+def image_filter(df:pd.DataFrame, max_width:int, max_height:int)->pd.DataFrame:
+    return df[(df['width'] <= max_width) & (df['height'] <= max_height)]
+
+def add_area_col(df:pd.DataFrame)->None:
+    df["area"] = df["height"]*df["width"]
+
+def image_area_sorter(df:pd.DataFrame)->None:
+    df = df.sort_values(by='area')
+
+def area_hist(df:pd.DataFrame)->None:
+    plt.figure(figsize=(10, 5))
+    plt.hist(df['area'], color='gray')
+    plt.title('Распределение площадей изображений')
+    plt.xlabel('Площадь (пиксели)')
+    plt.ylabel('Частота')
+    plt.grid(axis='y', alpha=0.75)
+    plt.show()
+
+# df_filtered = filter_images(df,500,500)
+# print(df_filtered)
+#
+# print(df_sorted)
