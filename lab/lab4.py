6e11e56b22265ca6fdf80db74b6b2da5cf4fc111
Keltas <inozemtseeev@gmail.com>
2024-11-15 21:05:07 +0400

    Добавлены описания к функциям

--- lab/lab4.py
+++ lab/lab4.py
@@ -5,6 +5,11 @@ import os
 
 
 def init_data_frame(annotation_path:str)-> pd.DataFrame:
+    """
+    Создает фрейм с абсолютными и относительными путями к изображениям.
+    :param annotation_path: Путь к файлу с аннотацией.
+    :return: Фрейм с абсолютными и относительными путями.
+    """
     if not os.path.exists(annotation_path):
         raise FileNotFoundError(f"Файл аннотации не найден: {annotation_path}")
     df = pd.read_csv(annotation_path)
@@ -12,6 +17,11 @@ def init_data_frame(annotation_path:str)-> pd.DataFrame:
     return df
 
 def add_h_w_d_cols(df:pd.DataFrame)->None:
+    """
+    Добавляет столбцы высоты, ширины и глубины изображения во фрейм.
+    :param df: Информация об изображениях.
+    :return: None
+    """
     h = []
     w = []
     d = []
@@ -25,19 +35,46 @@ def add_h_w_d_cols(df:pd.DataFrame)->None:
     df.insert(4, "depth", pd.Series(d), True)
 
 def h_w_d_statistical_info(df:pd.DataFrame)->None:
+    """
+    Выводит статистическую информацию для столбцов высоты, ширины и глубины.
+    :param df: Информация об изображениях.
+    :return: None
+    """
     stats = df[['height', 'width', 'depth']].describe()
     print(stats)
 
 def image_filter(df:pd.DataFrame, max_width:int, max_height:int)->pd.DataFrame:
+    """
+    Создает отфильтрованный по ширине и высоте фрейм с информацией об изображениях.
+    :param df: Информация об изображениях.
+    :param max_width: Максимальная ширина изображения.
+    :param max_height: Максимальная высота изображения.
+    :return: Отфильтрованный фрейм.
+    """
     return df[(df['width'] <= max_width) & (df['height'] <= max_height)]
 
 def add_area_col(df:pd.DataFrame)->None:
+    """
+    Добавляет столбец с площадями изображений.
+    :param df: Информация об изображениях.
+    :return: None
+    """
     df["area"] = df["height"]*df["width"]
 
 def image_area_sorter(df:pd.DataFrame)->None:
+    """
+    Сортирует информацию об изображениях по возрастанию площади
+    :param df: Информация об изображениях.
+    :return: None
+    """
     df = df.sort_values(by='area')
 
 def area_hist(df:pd.DataFrame)->None:
+    """
+    Выводит гистограмму площадей изображений.
+    :param df: Информация об изображениях.
+    :return: None
+    """
     plt.figure(figsize=(10, 5))
     plt.hist(df['area'], color='gray')
     plt.title('Распределение площадей изображений')
